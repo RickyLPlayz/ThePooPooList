13e7b3e9e36135591146b64e75f5b5cd7aa65c11
RickyLPlayz <riccardolanza2014@gmail.com>
2026-03-20 12:10:26 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,13 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Admin from './pages/Admin.js';
+import Manage from './pages/manage.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
+    { path: '/admin', component: Admin },
+    { path: '/manage', component: Manage },
 ];
