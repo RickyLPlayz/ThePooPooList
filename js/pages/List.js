79a5b3d4f04d9196d21a386771de5239b3b9519a
RickyLPlayz <riccardolanza2014@gmail.com>
2026-03-20 11:36:03 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -121,7 +121,10 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, we will not be accepting any of its records for any longer
+                    </p>
+                    <p>
+                        Level must have brown as its main colour theme.
                     </p>
                 </div>
             </div>
